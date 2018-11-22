e20a5ac34a9574422db8ee3091bb1ebce7101e1d
hodolski7615 <hodolski7615@gmail.com>
2018-11-22 15:30:26 +0900

    버튼 추가

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
     public Text ui_text;
+    public Text Answer1, Answer2, Answer3;
 	// Use this for initialization
 	void Start () {
         int a = Random.Range(0, 100);
         int b = Random.Range(0, 100);
         ui_text.text = a.ToString() + " + " + b.ToString();
+
+        Answer1.text = (a + b).ToString();
+        Answer2.text = (a + b + 1).ToString();
+        Answer3.text = (a + b - 1).ToString();
     }
 	
 	// Update is called once per frame
