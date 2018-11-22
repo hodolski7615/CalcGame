056ac584c3d67e63e7c36c95d66b408c844a8c53
kiwiade <bluestar373@naver.com>
2018-11-22 16:08:36 +0900

    버튼누르면 debug log 출력

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,15 +7,43 @@ public class GameManager : MonoBehaviour {
 
     public Text ui_text;
     public Text Answer1, Answer2, Answer3;
+
+    private int a;
+    private int b;
 	// Use this for initialization
 	void Start () {
-        int a = Random.Range(0, 100);
-        int b = Random.Range(0, 100);
+        a = Random.Range(0, 100);
+        b = Random.Range(0, 100);
         ui_text.text = a.ToString() + " + " + b.ToString();
 
         Answer1.text = (a + b).ToString();
         Answer2.text = (a + b + 1).ToString();
         Answer3.text = (a + b - 1).ToString();
+    }
+
+    public bool IsAnswerToAdd(int number1, int number2, int userAnswer)
+    {
+        return (number1 + number2) == userAnswer;
+    }
+
+    public void IfAnswerCorrect_ButtonColorChange(int buttonNum)
+    {
+        int answer = 0;
+        if (buttonNum == 1)
+            answer = int.Parse(Answer1.text);
+        else if (buttonNum == 2)
+            answer = int.Parse(Answer2.text);
+        else if (buttonNum == 3)
+            answer = int.Parse(Answer3.text);
+
+        if (IsAnswerToAdd(a, b, answer))
+        {
+            print("correct");
+        }
+        else
+        {
+            print("wrong");
+        }
     }
 	
 	// Update is called once per frame
