eaaebfd8ded91dbc5f1801ccff9a6aab4a1f7589
kiwiade <bluestar373@naver.com>
2018-11-22 15:23:04 +0900

    GameManager 수정, Game씬 수정(Canvas - Text 생성)

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    public Text ui_text;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        int a = Random.Range(0, 100);
+        int b = Random.Range(0, 100);
+        ui_text.text = a.ToString() + " + " + b.ToString();
+    }
 	
 	// Update is called once per frame
 	void Update () {
